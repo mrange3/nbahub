f7e3e1d39dc3d9330b98b82ee3e260b3695a68fd
mrange3 <matthew.range91@gmail.com>
2019-02-12 22:15:02 -0500

    added logos

--- javascript.js
+++ javascript.js
@@ -6,6 +6,14 @@ var dd = today.getDate();
 var mm = today.getMonth() + 1; //January is 0!
 var yyyy = today.getFullYear();
 
+const monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+
+todaySchedule = monthNames[mm - 1] + " " + dd + ", " + yyyy
+
+$("#date").text(todaySchedule);      // Append the new elements 
+
 if (dd < 10) {
   dd = '0' + dd;
 }
@@ -14,57 +22,122 @@ if (mm < 10) {
   mm = '0' + mm;
 }
 
-today = yyyy  + mm  + dd;
+// $("#yesterday").click(function() {
+//   dd--;
+// });
 
+// $("#tomorrow").click(function() {
+//   dd++;
+// });
+
+today = yyyy + mm + dd;
 //====================== Get Request for Todays Games ============
-var gameUrl = "https://api.mysportsfeeds.com/v2.0/pull/nba/2018-2019/date/" + today + "/games.json"
+var gameUrl = "https://api.mysportsfeeds.com/v2.0/pull/nba/current/date/" + today + "/games.json"
 
-var api = "d5ebfa47-c23c-459b-b340-f9d0bc"
+var api = config.MY_KEY
 
 $.ajax
-({
-  type: "GET",
-  url: gameUrl,
-  dataType: 'json',
-  headers: {
-    "Authorization": "Basic " + btoa(api + ":" + "MYSPORTSFEEDS")
-  },
+  ({
+    type: "GET",
+    url: gameUrl,
+    dataType: 'json',
+    headers: {
+      "Authorization": "Basic " + btoa(api + ":" + "MYSPORTSFEEDS")
+    },
 
-})
-.then(function (response) {
-    console.log(response); 
-    console.log(response.games[0].schedule);
+  })
+  .then(function (response) {
+    console.log(response);
 
-    console.log(response.games[0].schedule.homeTeam.abbreviation);
-    console.log(response.games[0].schedule.awayTeam.abbreviation);
+    for (i = 0; i < response.games.length; i++) {
 
-    var dateFromAPI = response.games[3].schedule.startTime;
+      var dateFromAPI = response.games[i].schedule.startTime;
+      var localDate = new Date(dateFromAPI);
+      var localDateString = localDate.toLocaleDateString(undefined, {
+        day: 'numeric',
+        month: 'short',
+        year: 'numeric'
+      })
 
-    var localDate = new Date(dateFromAPI);
-    var localDateString = localDate.toLocaleDateString(undefined, {  
-        day : 'numeric',
-        month : 'short',
-        year : 'numeric'
-    })
-   
-    var localTimeString = localDate.toLocaleTimeString(undefined, {
+      var localTimeString = localDate.toLocaleTimeString(undefined, {
         hour: '2-digit',
         minute: '2-digit',
-    })
-    console.log(localTimeString);
+      })
 
-    var hTeam = response.games[3].schedule.homeTeam.abbreviation;
-    var aTeam = response.games[3].schedule.awayTeam.abbreviation;
-    var aScore = response.games[3].score.awayScoreTotal;
-    var hScore = response.games[3].score.homeScoreTotal;
+      var hTeam = response.games[i].schedule.homeTeam.abbreviation;
+      var aTeam = response.games[i].schedule.awayTeam.abbreviation;
+      var aScore = response.games[i].score.awayScoreTotal;
+      var hScore = response.games[i].score.homeScoreTotal;
+      var status = response.games[i].schedule.playedStatus;
+      var quarter = response.games[i].score.currentQuarter
+
+      function myTime(time) {
+        var hr = ~~(time / 3600);
+        var min = ~~((time % 3600) / 60);
+        var sec = time % 60;
+        var sec_min = "";
+        if (hr > 0) {
+           sec_min += "" + hrs + ":" + (min < 10 ? "0" : "");
+        }
+        sec_min += "" + min + ":" + (sec < 10 ? "0" : "");
+        sec_min += "" + sec;
+        return sec_min;
+      }
+      var timeRemaining = (myTime(response.games[i].score.currentQuarterSecondsRemaining));
+      
+      if (timeRemaining== "0:00") {
+        timeRemaining = ""
+      } else {
+        timeRemaining = timeRemaining + " Q"
+      }
+
+      if (quarter==null) {
+        quarter = ""
+      }
+
+      if (status == "COMPLETED_PENDING_REVIEW" || status == "COMPLETED") {
+        status = "FINAL";
+      }
+
+      if (status == "UNPLAYED") {
+        status = "";
+      }
+
+      if (aScore == null) {
+        aScore = 0;
+        hScore = 0;
+      }
+  
+      var htmlString = '<div class="card bg-white text-dark w-20 border border-dark rounded m-1 mb-1" style="width: 235px; height: 145px;">';
+      htmlString += '<div class="row">';
+      htmlString += '<p class="col-4 ml-2 text-left" style=" font-size: 15px;"><small>' + localTimeString + '</small></p>';
+      htmlString += '<p class="text-danger text-center col "style=" font-size: 15px;"><small>' + timeRemaining  + quarter+ '</small></p>';
+      htmlString += '<p class="text-danger col mr-2 text-right"><small>' + status + '</small></p>';
+      htmlString += '</div>';
+      htmlString += '<div class="row">';
+      htmlString += '<p class="  ml-2 col"><img src="images/logos/'+ aTeam + '.png" height="45px" width="45px"></p>'
+      htmlString += '<p class=" col " style="font-family: Kanit, sans-serif;">' + aTeam + '</p>';
+      htmlString += '<p class=" col "></p>';
+      htmlString += '<p class=" col mr-2 text-right" style="font-family: Kanit, sans-serif;"><strong>' + aScore + '</strong></p>';
+      htmlString += '</div>';
+      htmlString += '<div class="row">';
+      htmlString += '<p class=" ml-2 mb-2 col"><img src="images/logos/' + hTeam+ '.png" height="45px" width="45px"></p>'
+      htmlString += '<p class=" col " style="font-family: Kanit, sans-serif;">' + hTeam + '</p>';
+      htmlString += '<p class=" col "></p>';
+      htmlString += '<p class=" col mr-2 text-right" style="font-family: Kanit, sans-serif;"><strong>' + hScore + '</strong></p>';
+      htmlString += '</div>';
+      htmlString += '<div class="row">';
+      // htmlString += '<p class="ml-2 col text-left"><small>' + timeRemaining  + quarter+ '</small></p>';
+      // htmlString += '<p class="text-danger d-inline m-1 col mr-2  text-right"><small>' + status + '</small></p>';
+      htmlString += '</div>';
+      htmlString += '</div>';
+      htmlString += '</div>';
+      htmlString += '</div>';
+      $("#schedule-holder").append(htmlString);      // Append the new elements 
+    }
 
 
-    $("#start0").text(localTimeString + "    " + response.games[3].schedule.playedStatus);
-    $("#away-team0").text(aTeam + "     " + aScore);
-    $("#home-team0").text(hTeam + "     " + hScore);
-
-})
-
+  });
 
 
 
